73d0c841d9fc0e30b3e254d441e3ec6c47a85837
Dan Mosora <dan@stitchdata.com>
2018-11-12 20:39:01 +0000

    Version 0.1.1, bump requests to 2.20.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-taboola',
-      version='0.1.0',
+      version='0.1.1',
       description='Singer.io tap for extracting data from the Taboola API',
       author='Fishtown Analytics',
       url='http://www.singer.io',
@@ -12,7 +12,7 @@ setup(name='tap-taboola',
       install_requires=[
           'singer-python==5.0.4',
           'backoff==1.3.2',
-          'requests==2.12.4',
+          'requests==2.20.0',
           'python-dateutil==2.6.0'
       ],
       entry_points='''
