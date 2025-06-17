b480f8e7c45591f31dc43ef820b62bdf9f9d681d
Sohail-qlik <jaideep.nair@qlik.com>
2025-06-17 17:59:31 +0530

    config changes

--- tap_taboola/__init__.py
+++ tap_taboola/__init__.py
@@ -255,6 +255,33 @@ def verify_account_access(access_token, account_id):
     LOGGER.info("Verified account access via token details endpoint.")
     return account_id
 
+def validate_config(config):
+    required_keys = ['username', 'password', 'account_id',
+                     'client_id', 'client_secret', 'start_date']
+    missing_keys = []
+    null_keys = []
+    has_errors = False
+
+    for required_key in required_keys:
+        if required_key not in config:
+            missing_keys.append(required_key)
+
+        elif config.get(required_key) is None:
+            null_keys.append(required_key)
+
+    if missing_keys:
+        LOGGER.fatal("Config is missing keys: {}"
+                     .format(", ".join(missing_keys)))
+        has_errors = True
+
+    if null_keys:
+        LOGGER.fatal("Config has null keys: {}"
+                     .format(", ".join(null_keys)))
+        has_errors = True
+
+    if has_errors:
+        raise RuntimeError
+
 
 def load_config(filename):
     config = {}
@@ -265,6 +292,8 @@ def load_config(filename):
     except:
         LOGGER.fatal("Failed to decode config file. Is it valid json?")
         raise RuntimeError
+
+    validate_config(config)
 
     return config
 
