cebf44b62fcc39b8f0d4ed6b24638c2b2bcdbd97
Prijen Khokhani <88327452+prijendev@users.noreply.github.com>
2026-02-12 14:25:08 +0530

    Bump tap version (#27)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="tap-taboola",
-      version="0.3.2",
+      version="1.0.0",
       description="Singer.io tap for extracting data from the Taboola API",
       author="Fishtown Analytics",
       url="http://www.singer.io",
